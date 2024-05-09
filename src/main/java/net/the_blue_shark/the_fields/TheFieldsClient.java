c6bdb46b392ab4ab334440c1ef3745568a782fff
the-blue-shark <naeyaertlukas5@gmail.com>
2024-05-09 20:28:43 +0200

    Initial commit

---
+++ src/main/java/net/the_blue_shark/the_fields/TheFieldsClient.java
@@ -0,0 +1,10 @@
+package net.the_blue_shark.the_fields;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheFieldsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/the_blue_shark/the_fields/TheFieldsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.the_blue_shark.the_fields;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TheFieldsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
